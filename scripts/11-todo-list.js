af4fd0d410cd58978cbfa6ae4c058192bd9c2647
johnbBoom <yohannesgaromsa051@gmail.com>
2025-10-18 23:52:04 +0300

    today i only work on left of the course

---
+++ scripts/11-todo-list.js
@@ -0,0 +1,15 @@
+const todoList=[];
+
+function addTodo(){
+ const inputElement= document.querySelector('.js-todo-input');
+ const name=inputElement.value;
+
+ todoList.push(name);
+ console.log(todoList);
+
+
+ inputElement.value='';
+
+
+
+}
